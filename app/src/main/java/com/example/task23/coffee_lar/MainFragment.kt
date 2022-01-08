c6a718b981378d01417ec10523a7ba51529a45ca
1_skat_1 <skat.apps@gmail.com>
2022-01-08 18:35:09 +0300

    edit recyclerView for coffee

--- app/src/main/java/com/example/task23/coffee_lar/MainFragment.kt
+++ app/src/main/java/com/example/task23/coffee_lar/MainFragment.kt
@@ -20,11 +20,11 @@ class MainFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater, R.layout.fragment_main, container, false)
 
         val coffies = mutableListOf<Coffee>()
-        coffies.add(Coffee(R.drawable.coffee1, "Latte Art Speciality", "Expresso with milk"))
-        coffies.add(Coffee(R.drawable.coffee2, "Long Black coffee", "Access water-milk"))
+        coffies.add(Coffee(R.drawable.coffee2, "Latte Art Speciality", "Expresso with milk"))
+        coffies.add(Coffee(R.drawable.coffee1, "Long Black coffee", "Access water-milk"))
 
-        val adapter = CoffeeAdapter(listOf<Coffee>(), requireContext())
-        binding.recyclerCoffee.layoutManager = LinearLayoutManager(context)
+        val adapter = CoffeeAdapter(coffies, requireContext())
+        binding.recyclerCoffee.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         binding.recyclerCoffee.adapter = adapter
         return binding.root
     }
